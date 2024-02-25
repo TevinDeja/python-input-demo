0acb151767a353786febac6cc210ede153e96f10
Tevin Deja <tevinowena@gmail.com>
2024-02-25 08:58:55 +0300

    Python Innit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Ask user for name, age, location
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where do you live? ")
+
+# Print personalized message
+print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
